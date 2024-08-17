f9ae91ad8da7925a59665d839ff6dad322379aa1
OkunrinmetaWebDevelopment <c.okunrinmetrix@gmail.com>
2024-08-17 09:55:24 +0400

    rag chat stream
    rag chat stream

--- chatgpt/src/app/[...url]/page.tsx
+++ chatgpt/src/app/[...url]/page.tsx
@@ -1,12 +1,48 @@
+import { ChatWrapper } from "@/component/ChatWrapper";
+import { ragChat } from "@/lib/rag-chat";
+import { redis } from "@/lib/redis";
+import { Component } from "react";
+
 interface PageProps{
     params:{
        url: string| string[] | undefined
     }
 }
 
-const Page = ({params}:PageProps) =>{
+
+function reconstructUrl({url}:{url:string[]}) {
+    const decodedComponents=url.map((component)=> decodeURIComponent(component))
+
+    return decodedComponents.join("/")
+}
+
+const Page = async ({params}:PageProps) =>{
     console.log(params);
-    return <p>Hello</p>
+
+    const reconstructedUrl= reconstructUrl({url: params.url as string[]})
+
+    const isAlreadyIndexed= await redis.sismember("indexed-urls",reconstructedUrl)
+
+    console.log("isAlreadyIndexed",isAlreadyIndexed)
+
+    const sessioniD="mock-session"
+
+    if (!isAlreadyIndexed) {
+
+        await ragChat.context.add({
+            type:"html",
+            source: reconstructedUrl,
+            config:{ chunkOverlap:50,chunkSize:200},
+        })
+
+        await redis.sadd("indexed-urls",reconstructedUrl)
+        
+    }
+    
+    
+
+
+    return <ChatWrapper sessioniD={sessioniD}></ChatWrapper>
 }
 
 export default Page
